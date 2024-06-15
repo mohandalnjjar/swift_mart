5b5085e9e5e57f0b87bd98faad95cefddf43ea94
mohand <mohanadalnjjar23@gmail.com>
2024-06-15 14:50:51 +0300

    link the app to firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,23 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:swift_mart/core/functions/app_theme_func.dart';
 import 'package:swift_mart/core/utils/services/app_router.dart';
 import 'package:swift_mart/features/theme/data/repos/theme_repo_impl.dart';
 import 'package:swift_mart/features/theme/presentation/managers/cubit/theme_cubit.dart';
+import 'package:swift_mart/firebase_options.dart';
 
-void main(List<String> args) {
+void main(List<String> args) async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
-    const SwiftMart(),
+    DevicePreview(
+      enabled: false,
+      builder: (context) => const SwiftMart(), 
+    ),
   );
 }
 
@@ -27,15 +37,15 @@ class SwiftMart extends StatelessWidget {
       child: BlocBuilder<ThemeCubit, ThemeCubitState>(
         builder: (context, state) {
           return MaterialApp.router(
-        
             title: 'Swift Mart',
             debugShowCheckedModeBanner: false,
             routerConfig: AppRouter.router,
-          
             theme: appThemeData(
-                isDark: BlocProvider.of<ThemeCubit>(context).themeMode,
-                context: context),
-                
+              isDark: BlocProvider.of<ThemeCubit>(context).themeMode,
+              context: context,
+            ),
+            locale: DevicePreview.locale(context),
+            builder: DevicePreview.appBuilder,
           );
         },
       ),
